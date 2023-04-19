befb6e451c8754c79e11794cb0e903161aaaba52
João Gabriel Oliveira Coelho <86790145+JoaoGabrielO@users.noreply.github.com>
2023-04-19 08:43:01 -0300

    Add files via upload

---
+++ EstruturaDeDados/EstruturaDinamica/ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -0,0 +1,81 @@
+package EstruturaDeDados.EstruturaDinamica.ListaDuplamenteEncadeada;
+
+import java.util.NoSuchElementException;
+
+public class ListaDuplamenteEncadeada {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    private class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public ListaDuplamenteEncadeada() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+
+        size--;
+    }
+}
